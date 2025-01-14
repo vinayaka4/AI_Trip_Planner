9f2b7ab16938f3dc07e6ae9369729150a5d3fbc1
DESKTOP-88L6VBB\91903 <hkvin4@gmail.com>
2025-01-13 19:57:30 -0500

    package.json

--- src/service/FirebaseConfig.jsx
+++ src/service/FirebaseConfig.jsx
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
     apiKey: "AIzaSyBdZATlOHsrpRXniHK7XZ_uh3UwnC-_beU",
     authDomain: "aitripplanner-e19f8.firebaseapp.com",
@@ -17,7 +12,10 @@ const firebaseConfig = {
     measurementId: "G-GTD2WC0H6D"
 };
 
-// Initialize Firebase
+
+
+
+
+
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-// const analytics = getAnalytics(app);
